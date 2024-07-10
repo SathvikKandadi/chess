6d6d6da22acb18dd5088e98e725bfd128ffb0a1a
Sathvik Kandadi <sathvikkandadi@gmail.com>
2024-07-10 16:43:12 +0530

    Added Chessboard to game page

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,20 +1,22 @@
 import { Chess } from "chess.js";
 
 export default function ChessBoard() {
-
     const game = new Chess();
     const chessBoard = game.board();
 
     return (
-        <div className="my-16 mx-16 border-2 border-white">
+        <div className="">
             {
                 chessBoard.map((row, i) => (
                     <div className="grid grid-cols-8" key={i}>
                         {
                             row.map((square, j) => (
-                                <div key={j} className={(i + j) % 2 === 0 ? 'bg-green-500' : 'bg-white'}>
+                                <div 
+                                    key={j} 
+                                    className={`${(i + j) % 2 === 0 ? 'bg-green-500' : 'bg-white'} h-12 w-12 md:h-16 md:w-16 lg:h-20 lg:w-20 xl:h-24 xl:w-24 flex items-center justify-center`}
+                                >
                                     <div className="flex items-center justify-center h-full">
-                                        {square ? `${square.color}${square.type}` : 'a'}
+                                        {square ? `${square.color}${square.type}` : ''}
                                     </div>
                                 </div>
                             ))
@@ -23,5 +25,5 @@ export default function ChessBoard() {
                 ))
             }
         </div>
-    )
+    );
 }
--- frontend/src/screens/GamePage.tsx
+++ frontend/src/screens/GamePage.tsx
@@ -14,9 +14,15 @@ export default function GamePage() {
         console.log("Message from server ", event.data)
     })
   return (
-    <div className='h-full w-full grid grid-cols-2'>
-        <ChessBoard/>
-        <div>Start</div>
-    </div>
+    <div className='h-screen w-screen grid grid-cols-3'>
+            <div className='grid col-span-3 md:col-span-2  flex justify-center items-center'>
+                <ChessBoard />
+            </div>
+            <div className='grid col-span-3 md:col-span-1 flex justify-center items-center'>
+                <button className="bg-green-500 text-white p-4 rounded hover:bg-green-600">
+                    Start
+                </button>
+            </div>
+        </div>
   )
 }
